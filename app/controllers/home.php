a558cf6bc5bebc7b132098881187ab8570646aa7
Igor <cableclark@gmail.com>
2018-08-16 16:37:33 +0200

    first commit

---
+++ app/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+ class Home extends Controller 
+ {
+
+     public function index ($name= 
+     "") {
+        $user =  $this->model("User");
+        $user->name= "Alex";
+        $this->view('home/index', ['name'=>$user->name]);
+     }
+   
+
+
+ }  
